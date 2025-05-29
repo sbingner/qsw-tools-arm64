e8edee3e0366cf2296f461dcb0d7b2f65bb03197
Sam Bingner <sam@bingner.com>
2025-05-28 23:10:06 -1000

    Update to support QSW 4.x

--- genpwd.py
+++ genpwd.py
@@ -1,8 +1,8 @@
 #/usr/bin/python
 import sys
 
-magic = "qSxWQpAN"
-salt = f"$6${magic}"
+magic30 = "qSxWQpAN"
+magic40 = "NQxWSpqA"
 
 try:
     from passlib.hash import sha512_crypt
@@ -17,13 +17,20 @@ except ImportError:
         sys.exit(1)
     crypt = _crypt.crypt
 
-def gen_enable_pwd(serial):
+def gen_enable_pwd(serial, salt):
     hash = crypt(serial, salt);
-    return f"{hash.split('$')[-1]}{hash.split('$')[2][-1]}"
+    return f"{hash[len(salt)+1:]}{hash[len(salt)+8]}"
 
 if __name__ == "__main__":
+    if len(sys.argv) == 3 and sys.argv[1] == "-4":
+        sys.argv.remove("-4")
+        magic = magic40
+    else:
+        magic = magic30
+
+    salt = f"$6${magic}"
     if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <serial number>")
+        print(f"Usage: {sys.argv[0]} [-4] <serial number>")
         sys.exit(1)
     serial = sys.argv[1]
-    print("Enable password:", gen_enable_pwd(serial))
+    print("Enable password:", gen_enable_pwd(serial, salt))
